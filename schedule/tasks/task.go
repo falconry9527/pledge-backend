6712e9dcfb16de94f28f217864376e249c1d9448
conry <kequan888@gmail.com>
2025-09-07 17:57:45 +0800

    UpdateTokenLogo

--- schedule/tasks/task.go
+++ schedule/tasks/task.go
@@ -33,14 +33,17 @@ func Task() {
 	s := gocron.NewScheduler()
 	s.ChangeLoc(time.UTC)
 
-	// 更新全部 poolbases/pooldata
+	// 更新全部 poolbases/pooldata/token_info (存在就更新，不存在就插入Token，ChainId，CreatedAt，UpdatedAt)
 	_ = s.Every(2).Minutes().From(gocron.NextTick()).Do(services.NewPool().UpdateAllPoolInfo)
 	// 访问BscPledgeOracle(间接访问 chainlink) ：  token_info.price
 	_ = s.Every(1).Minute().From(gocron.NextTick()).Do(services.NewTokenPrice().UpdateContractPrice)
+	// 更新名称 token_info.symbol （根据合约地址）
 	_ = s.Every(2).Hours().From(gocron.NextTick()).Do(services.NewTokenSymbol().UpdateContractSymbol)
+	// 更新图标 Logo（根据合约地址）
 	_ = s.Every(2).Hours().From(gocron.NextTick()).Do(services.NewTokenLogo().UpdateTokenLogo)
+	//
 	_ = s.Every(30).Minutes().From(gocron.NextTick()).Do(services.NewBalanceMonitor().Monitor)
-	//_ = s.Every(30).Minutes().From(gocron.NextTick()).Do(services.NewTokenPrice().SavePlgrPrice)
+	//
 	_ = s.Every(30).Minutes().From(gocron.NextTick()).Do(services.NewTokenPrice().SavePlgrPriceTestNet)
 	<-s.Start() // Start all the pending jobs
 
